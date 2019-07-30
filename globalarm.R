352b4b1f0fb7d18b9a455b8e2111d7558b21e275
vriffel <viniciusricardoriffel@gmail.com>
2019-07-30 15:08:14 -0300

    a

--- globalarm.R
+++ globalarm.R
@@ -2,9 +2,10 @@ library(tidyverse)
 library(jsonlite)
 library(stringr)
 
-myData <- fromJSON(url("https://raw.githubusercontent.com/dataarts/armsglobe/master/categories/All.json"))
+#myData <- fromJSON(url("https://raw.githubusercontent.com/dataarts/armsglobe/master/categories/All.json"))
 #saveRDS(myData, file = "myData.RDS")
-#myData <- readRDS("myDataA)
+myData <- readRDS("myData.RDS")
+
 da <- unlist(myData)
 db <- as.vector(da)
 ## i importação
@@ -26,9 +27,3 @@ for (i in 1:19) {
 }
 year <- rep(c(1992:2010), times = times.year)
 all <- data.frame("year" = year, "imp" = imp, "wc" = wc, "e" = e, "v" = v)
-
-#-----------------------------------------------------------------------
-
-br.idx <- which(all$i == "Brazil")
-br <- all[br.idx, ]
-head(br)
