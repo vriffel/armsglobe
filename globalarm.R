1fd89841826ec13eab20c77e65029935682916ac
Vinicius Riffel <viniciusricardoriffel@gmail.com>
2019-08-01 21:34:59 -0300

    a

--- globalarm.R
+++ globalarm.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(jsonlite)
 library(stringr)
 
-myData <- fromJSON(url("https://raw.githubusercontent.com/dataarts/armsglobe/master/categories/All.json"))
-#saveRDS(myData, file = "myData.RDS")
-#myData <- readRDS("myDataA)
-da <- unlist(myData)
+#mydata <- fromJSON(url("https://raw.githubusercontent.com/dataarts/armsglobe/master/categories/All.json"))
+#saveRDS(mydata, file = "mydata.RDS")
+mydata <- readRDS("mydata.RDS")
+da <- unlist(mydata)
 db <- as.vector(da)
 ## i importação
 imp.idx <- str_detect(string = names(da), pattern = "\\.i")
@@ -21,7 +21,7 @@ e <- db[e.idx]
 v <- as.numeric(db[v.idx])
 times.year <- numeric(19)
 for (i in 1:19) {
-    yr <- nrow(myData$timeBins$data[[i]])
+    yr <- nrow(mydata$timeBins$data[[i]])
     times.year[i] <- yr
 }
 year <- rep(c(1992:2010), times = times.year)
